2d73ba31b80f668fada9247804ea8b8f58823025
Evgen7985 <Evgen7985@yandex.ru>
2024-04-15 13:27:54 +0500

    Второе задание по JS

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,106 @@
-function getArrayParams(...arr) {
+'use strict'
 
-  return { min: min, max: max, avg: avg };
+// Задача №1;
+
+function getArrayParams(...arr) {
+	let min = arr[0];
+	let max = arr[0];
+	let sum = 0;
+
+
+	for (let i = 0; i < arr.length; i++) {
+		if (min > arr[i]) {
+			min = arr[i];
+		}
+		if (max < arr[i]) {
+			max = arr[i];
+		}
+		sum += arr[i];
+	}
+
+	const avg = Number((sum / arr.length).toFixed(2));
+
+	return {
+		min: min,
+		max: max,
+		avg: avg
+	};
 }
+
+
+// Задача №2;
+
 
 function summElementsWorker(...arr) {
+	if (!arr.length > 0) {
+		return 0;
+	}
 
+	let sum = arr.reduce((currentSum, currentNumber) => {
+		return currentSum + currentNumber
+	}, 0)
+	return sum;
 }
 
-function differenceMaxMinWorker(...arr) {
 
+function differenceMaxMinWorker(...arr) {
+	if (!arr.length > 0) {
+		return 0;
+	}
+
+	let max = Math.max(...arr);
+	let min = Math.min(...arr);
+
+	return max - min;
 }
 
 function differenceEvenOddWorker(...arr) {
+	if (!arr.length > 0) {
+		return 0;
+	}
 
+	let sumEvenElement = 0;
+	let sumOddElement = 0;
+
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 == 0) {
+			sumEvenElement += arr[i];
+		} else {
+			sumOddElement += arr[i];
+		}
+	}
+	return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
+	if (!arr.length > 0) {
+		return 0;
+	}
 
+	let sumEvenElement = 0;
+	let countEvenElement = 0;
+
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 == 0) {
+			sumEvenElement += arr[i];
+			countEvenElement++;
+		}
+	}
+	return sumEvenElement / countEvenElement;
 }
 
-function makeWork (arrOfArr, func) {
 
+//Задача №3;
+
+function makeWork(arrOfArr, func) {
+	let maxWorkResult = -Infinity;
+
+	for (let i = 0; i < arrOfArr.length; i++) {
+		let max = func(...arrOfArr[i]);
+		if (maxWorkResult < max) {
+			maxWorkResult = max;
+		}
+	}
+	return maxWorkResult;
 }
+
