f4f91d9c47c79957fac2e94c7d8a24a1f26097b0
Evgen7985 <Evgen7985@yandex.ru>
2024-04-25 15:45:52 +0500

    Шестое домашнее задание по JS

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,66 @@
-﻿
+﻿"use strict";
+
+
+// Задание 1
+
+function parseCount(element) {
+	let parseElement = Number.parseFloat(element);
+	// debugger
+	if (!parseElement) {
+		throw new Error("Невалидное значение");
+	} else return (parseElement);
+}
+
+function validateCount(element) {
+
+	try {
+		return parseCount(element);
+	} catch (error) {
+		return error;
+	}
+
+}
+
+
+
+// Задание 2
+
+
+class Triangle {
+	constructor(a, b, c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+
+		console.log(a, b, c)
+		if (a + b < c || a + c < b || b + c < a) {
+			throw new Error("Треугольник с такими сторонами не существует");
+		}
+	}
+
+	get perimeter() {
+		return Number(this.a + this.b + this.c);
+	}
+
+	get area() {
+		let p = (this.a + this.b + this.c) / 2;
+		return Number((Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))).toFixed(3));
+	}
+}
+
+
+function getTriangle(a, b, c) {
+	try {
+		return new Triangle(a, b, c);
+	} catch (err) {
+		return {
+			get perimeter() {
+				return "Ошибка! Треугольник не существует";
+			},
+
+			get area() {
+				return "Ошибка! Треугольник не существует";
+			},
+		};
+	}
+}
